04c52b4e161b86dda5c44d06734e64a69da39a74
Derek Meduri <derekmeduri@gmail.com>
2024-06-08 14:19:33 -0400

    initial vite build

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, "src"),
-  resolve: {
-    alias: {
-      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
-    },
-  },
   server: {
     port: 3000,
-    open: true,
   },
 });
